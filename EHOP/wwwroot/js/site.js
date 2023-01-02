313a9bdea01197cafd12becce13af2ccc0079f95
Komal Afzaal <komalafzaal24@gmail.com>
2023-01-03 01:33:50 +0500

    product page basic frontend added

--- EHOP/wwwroot/js/site.js
+++ EHOP/wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function addToCart(id) {
+	var addToCartButton = document.getElementById("addToCart-" + id);
+	document.getElementById("addToCart-" + id).innerHTML = "";
+	document.getElementById("addToCart-" + id).innerHTML = "Added";
+	document.getElementById("addToCart-" + id).disabled = true;
+}
