37df8a61a1e7c41e058d2f24e8547dd4f81b6ecd
Komal Afzaal <komalafzaal24@gmail.com>
2023-01-25 00:24:17 +0500

    login signup frontend change

--- EHOP/wwwroot/js/site.js
+++ EHOP/wwwroot/js/site.js
@@ -1,6 +1,39 @@
-﻿function addToCart(id) {
-	var addToCartButton = document.getElementById("addToCart-" + id);
-	document.getElementById("addToCart-" + id).innerHTML = "";
-	document.getElementById("addToCart-" + id).innerHTML = "Added";
-	document.getElementById("addToCart-" + id).disabled = true;
-}
+﻿//function addToCart(id) {
+//	var addToCartButton = document.getElementById("addToCart-" + id);
+//	document.getElementById("addToCart-" + id).innerHTML = "";
+//	document.getElementById("addToCart-" + id).innerHTML = "Added";
+//	document.getElementById("addToCart-" + id).disabled = true;
+//}
+
+$(function () {
+    $(".btn").click(function () {
+        $(".form-signin").toggleClass("form-signin-left");
+        $(".form-signup").toggleClass("form-signup-left");
+        $(".frame").toggleClass("frame-long");
+        $(".signup-inactive").toggleClass("signup-active");
+        $(".signin-active").toggleClass("signin-inactive");
+        $(".forgot").toggleClass("forgot-left");
+        $(this).removeClass("idle").addClass("active");
+    });
+});
+
+$(function () {
+    $(".btn-signup").click(function () {
+        $(".nav").toggleClass("nav-up");
+        $(".form-signup-left").toggleClass("form-signup-down");
+        $(".success").toggleClass("success-left");
+        $(".frame").toggleClass("frame-short");
+    });
+});
+
+$(function () {
+    $(".btn-signin").click(function () {
+        $(".btn-animate").toggleClass("btn-animate-grow");
+        $(".welcome").toggleClass("welcome-left");
+        $(".cover-photo").toggleClass("cover-photo-down");
+        $(".frame").toggleClass("frame-short");
+        $(".profile-photo").toggleClass("profile-photo-down");
+        $(".btn-goback").toggleClass("btn-goback-up");
+        $(".forgot").toggleClass("forgot-fade");
+    });
+});
