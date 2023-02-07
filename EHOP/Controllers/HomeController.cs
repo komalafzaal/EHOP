2af758d2e15b8228cf283332dffea534421a283d
Komal Afzaal <komalafzaal24@gmail.com>
2023-02-08 03:48:44 +0500

    home page seller file added  and file uploader function added

--- EHOP/Controllers/HomeController.cs
+++ EHOP/Controllers/HomeController.cs
@@ -1,17 +1,17 @@
 ﻿using EHOP.Models;
+using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
 using System.Diagnostics;
+using System.Web;
 
 namespace EHOP.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        private readonly IWebHostEnvironment Environment;
 
         public IActionResult homePage()
         {
@@ -24,5 +24,33 @@ namespace EHOP.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult HomePageSeller()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult HomePageSeller(List<IFormFile> postedFiles)
+        {
+            string wwwPath = Directory.GetCurrentDirectory();
+            string path = Path.Combine(wwwPath, "uploads");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            foreach (var file in postedFiles)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var pathWithFileName = Path.Combine(path, fileName);
+                using (FileStream stream = new FileStream(pathWithFileName, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                    ViewBag.Message = "file uploaded successfully";
+                }
+            }
+            return View();
+        }
+
     }
 }
--- EHOP/Program.cs
+++ EHOP/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Login}/{action=BuyerLogin}/{id?}");
+    pattern: "{controller=Home}/{action=HomePageSeller}/{id?}");
 
 app.Run();
